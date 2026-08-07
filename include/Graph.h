cb6def97717b81b2dd7e6a6ca22a1bc2d419bde8
Aida Ramezany <ramezanyaida84@gmail.com>
2026-08-07 17:05:44 +0330

    Update Graph.h

--- include/Graph.h
+++ include/Graph.h
@@ -31,7 +31,6 @@ public:
     bool addEdge(const Edge& edge);
     bool removeVertex(const std::string& vertexName);
     bool removeEdge(const std::string& source, const std::string& destination);
-    Vertex* findVertex(const std::string& vertexName);
     const Vertex* findVertex(const std::string& vertexName) const;
     bool hasVertex(const std::string& vertexName) const;
     bool hasEdge(const std::string& source, const std::string& destination) const;
